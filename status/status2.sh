f9389ea8316ed1bf834fa81c85e86179704ed16f
Eric Dvorsak <eric@dvorsak.fr>
2017-11-19 04:55:02 +0100

    update status script

--- status/status2.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-cd status-react
-react-native start
--- status/status3.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-cd status-react
-adb reverse tcp:8081 tcp:8081
-adb reverse tcp:3449 tcp:3449
-adb reverse tcp:4567 tcp:4567
-react-native run-android
