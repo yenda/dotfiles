1d20de59d5295aee2132e89edf3117a22b4dcae7
Eric Dvorsak <eric@dvorsak.fr>
2017-11-19 04:56:42 +0100

    update lein profile

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.15.1"]
+                  [refactor-nrepl "2.3.1"]
                   [lein-ancient "0.6.10"]
                   [lein-pprint "1.1.2"]
                   [validate "0.2.0"]]}}
